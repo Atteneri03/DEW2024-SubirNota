97ad6217c28de62c423d7d79cfa798a2de27dd4d
Atteneri <atteeste2003@gmail.com>
2024-01-25 12:01:34 +0000

    add libros.json

--- index.js
+++ index.js
@@ -8,18 +8,49 @@ app.listen(PORT, ()=>{
     console.log("El servidor está escuchando en el puerto " + PORT);
 });
 
+const LIBROS = require("./libros");
+
 //Con esto se indica que si se accede a la ruta raíz ‘/’ se ejecuta la función => indicada
 app.get("/", (req,res)=>{
     res.send("hola mundo!");
 });
 
-app.get("/api/clientes", (req,res)=>{
-    res.send("api de clientes con metodo get");
+app.get("/api/libros", (req,res)=>{
+    let libros_propiedades = LIBROS.map(libro => {
+        return {
+            id: libro.id,
+            titulo: libro.titulo,
+            autor: libro.autor
+        }
+    });
+    
+    
+    res.json({
+        success:true,
+        message: "Listado de libros",
+        data: {
+            count: libros_propiedades.length,
+            libros: libros_propiedades
+        }
+    });
 });
 
-app.get("/api/clientes/:id", (req,res)=>{
+app.get("/api/libros/:id", (req,res)=>{
     let id = req.params.id;
-    res.send("Buscar cliente con la id=" + id);
+    let filtro = LIBROS.filter(libro => libro.id == id);
+    if(filtro.length>0){
+        res.json({
+            success:true,
+            message: "Libro encontrado con id: "+ id,
+            data: filtro[0]
+        });
+    } else {
+        res.status(404).json({
+            success: false,
+            error_code: 4321,
+            message: "No se encuentra ningún libro con el id: " + id
+        });
+    }
 });
 
 app.use(express.static("public"));
